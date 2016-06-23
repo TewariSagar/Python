2e03bc324da8337a31a473c349bd1d06cdf7238b
fsociality <sagar9911102314@gmail.com>
2016-06-23 01:00:18 -0400

    Everything on numbers and operators

---
+++ numbers.py
@@ -0,0 +1,35 @@
+#four number types int, long, float, complex
+
+#type(var) returns the type of the variable
+
+num1 = 2.5
+print type(num1)
+
+#we can do the following
+#4>5
+#6<2
+#6==6
+#6!=6
+
+print (3/2)
+print (3/2.0)
+
+#power has the highest priority
+#divison and multiplication has the next highest priority
+#subtraction and addition have the next lease priority
+
+#number type conversions
+
+print int(1.5)
+print int(1.6)
+
+print float(2)
+
+#absolute is used to calculate the distance  of the argument value  to 0
+
+print abs(-2)
+
+#max() min() functions are used to find maximum and minimum
+
+print max(1,10)
+
