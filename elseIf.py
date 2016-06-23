239d0e2b4fe355e875d89e51c2494d43bc15b826
fsociality <sagar9911102314@gmail.com>
2016-06-23 17:34:34 -0400

    else if conditional loop

---
+++ elseIf.py
@@ -0,0 +1,14 @@
+#here elif is similar to else if
+
+x =5 
+if x > 5:
+	print "x is greater than 5"
+elif x < 5:
+	print "x is less than 5"
+else:
+	print "x is equal to 5"
+
+y = 100
+
+if y > 100 or  y == 100:
+	print "y is greater than or equal to 100"
