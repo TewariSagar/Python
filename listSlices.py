a11ebb7e707a2964e0fefcf2811dba066e07dbaa
fsociality <sagar9911102314@gmail.com>
2016-06-23 14:50:50 -0400

    slicing like string

---
+++ listSlices.py
@@ -0,0 +1,9 @@
+#all the rules of string slice apply to list slice
+
+list = [1,2,3,4,5,"a","bc"]
+
+#slice from 0 upto 3 but ont including 3
+print list[0:3]
+
+#we always put indexes in the order of traversing the list left to right
+
