9d668f706f9dcb7062e6a063b65aa2923306f5a1
fsociality <sagar9911102314@gmail.com>
2016-06-23 01:06:16 -0400

    basic info on boolean

---
+++ boolean.py
@@ -0,0 +1,23 @@
+print 1==1
+print 1==2
+
+print "python" == "p"
+
+#the and, not, or expressions are written with these keywords
+
+print (1==1) and (2==3)
+
+#some values always evalute to false
+#None
+#0
+#0.0
+#0L
+#0j
+#empty string
+#empty list
+#empty tuple
+#empty dictionary
+
+#bool() is used to check the boolean value of the following
+print bool(1)
+print bool("")
