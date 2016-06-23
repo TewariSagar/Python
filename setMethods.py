736980fca5549ea71d69688c0805aa45ed6482c2
fsociality <sagar9911102314@gmail.com>
2016-06-23 15:13:51 -0400

    some more methods for sets

---
+++ setMethods.py
@@ -0,0 +1,22 @@
+#find common elements between two sets
+
+set1 = {1,22,33,44,5}
+set2 = {33,0,100,999}
+
+print set1.intersection(set2)
+#elements that set1 has and set2 doesnt
+
+print set1.difference(set2)
+
+#unify two sets
+
+print set1.union(set2)
+
+#remove the random element in the set
+
+print set1.pop()
+
+#clear the set
+print set1.clear()
+
+
