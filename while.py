de046efcb78688d3f08edb38c2a16c90f5d78862
fsociality <sagar9911102314@gmail.com>
2016-06-23 19:15:08 -0400

    basic while syntax

---
+++ while.py
@@ -0,0 +1,17 @@
+x = 1
+
+while x < 10:
+	print x
+	x = x + 1 
+
+#while loop with else statement. here the else statement executes when the while condition beomes false
+
+y = 5
+
+while y < 7:
+	print y
+	y=  y + 1
+else:
+	print "y is equal to %d now " % y
+
+
